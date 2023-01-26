c7b191ad26675a88913e33f5c55a30a28d1bb406
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2023-01-26 22:41:34 +0530

    Environment Variables

--- 05_Natours_API/server.js
+++ 05_Natours_API/server.js
@@ -1,6 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
-const port = 8888;
+// console.log(app.get('env'));
+// console.log(process.env);
+
+const port = process.env.PORT || 8888;
 app.listen(port, () => {
   console.log(`App Running on Port: ${port}...`);
 });
