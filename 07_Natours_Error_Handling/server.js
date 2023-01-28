253fe966512950850edcba9904afb3d53e664a15
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2023-01-28 21:10:17 +0530

    Starting Code

---
+++ 07_Natours_Error_Handling/server.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
+const app = require('./app');
+
+const DB = process.env.DATABASE_CLOUD.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    // console.log(con.connections);
+    console.log('DB Connection Successful !');
+  })
+  .catch((err) => console.log(`Error Connecting to DB: ${err}`));
+
+// console.log(app.get('env'));
+// console.log(process.env);
+
+const port = process.env.PORT || 8888;
+app.listen(port, () => {
+  console.log(`App Running on Port: ${port}...`);
+});
