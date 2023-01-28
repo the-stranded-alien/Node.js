285ddeb980f641b7f84018c41e3f63accc533175
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2023-01-28 13:12:12 +0530

    Aliasing

--- 06_Natours_Mongo/routes/tourRoutes.js
+++ 06_Natours_Mongo/routes/tourRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const tourController = require('./../controllers/tourController');
+const tourController = require('../controllers/tourController');
 
 const router = express.Router();
+
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
 
 router
   .route('/')
