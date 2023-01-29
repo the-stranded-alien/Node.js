f42385b719cf6e9743e6f871b5f570582c578b29
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2023-01-29 14:28:43 +0530

    Error Handling

--- 07_Natours_Error_Handling/app.js
+++ 07_Natours_Error_Handling/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -25,5 +27,21 @@ app.use((req, res, next) => {
 // 2) Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// Route Handler for Invalid Routes
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+  // next(err);
+
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
--- 07_Natours_Error_Handling/server.js
+++ 07_Natours_Error_Handling/server.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('Unhandled Exception! Shutting Down...');
+  console.log(err.name, err.message);
+  // console.log(err);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
@@ -19,13 +26,22 @@ mongoose
   .then((con) => {
     // console.log(con.connections);
     console.log('DB Connection Successful !');
-  })
-  .catch((err) => console.log(`Error Connecting to DB: ${err}`));
+  });
+// .catch((err) => console.log(`Error Connecting to DB: ${err}`));
 
 // console.log(app.get('env'));
 // console.log(process.env);
 
 const port = process.env.PORT || 8888;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App Running on Port: ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('Unhandled Rejection! Shutting Down...');
+  console.log(err.name, err.message);
+  // process.exit(1);
+  server.close(() => {
+    process.exit(1);
+  });
 });
