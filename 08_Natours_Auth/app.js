65438ed9a7883fe56580709d811b0c833e8cf94d
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2023-02-01 18:23:17 +0530

    Security Headers and Data Sanitization

--- 08_Natours_Auth/app.js
+++ 08_Natours_Auth/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,10 +13,16 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) Global Middlewares
+
+// Set Security HTTP Headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests for same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -22,9 +31,19 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body Parser, Reading Data from Body into req.body, also limits body size
+app.use(express.json({ limit: '10kb' }));
+
+// Data Sanitization Against NoSQL Query Injection
+app.use(mongoSanitize());
+
+// Data Sanitization Against Cross Site Scripting Attacks (XSS)
+app.use(xss());
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test Middleware
 app.use((req, res, next) => {
   console.log('Hello from the Middleware !!');
   next();
