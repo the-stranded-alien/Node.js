7f130078c40ab126e23de78fb5b05306194e2c99
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2023-02-02 15:17:51 +0530

    Nested Routes

--- 09_Natours_Data_Model/routes/tourRoutes.js
+++ 09_Natours_Data_Model/routes/tourRoutes.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
+// const reviewController = require('../controllers/reviewController');
+const reviewRouter = require('./reviewRoutes');
 
 const router = express.Router();
+
+// router
+//   .route('/:tourId/reviews')
+//   .post(
+//     authController.protect,
+//     authController.restrictTo('user'),
+//     reviewController.createReview
+//   );
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap')
