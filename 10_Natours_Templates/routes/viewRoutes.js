03e2dab3b6f2bfd765800a8409097135fd94c386
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2023-02-03 19:47:57 +0530

    Code Restructuring

---
+++ 10_Natours_Templates/routes/viewRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const viewsController = require('../controllers/viewsController');
+
+const router = express.Router();
+
+// router.get('/', (req, res) => {
+//   res.status(200).render('base', {
+//     tour: 'The Forest Hiker',
+//     user: 'Jonas',
+//   });
+// });
+router.get('/', viewsController.getOverview);
+
+router.get('/tour', viewsController.getTour);
+
+module.exports = router;
