23464a1128163a0a31f1b5d7adc7ac5332cc7355
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2023-02-03 19:39:45 +0530

    Extending Base Template witth Blocks

--- 10_Natours_Templates/app.js
+++ 10_Natours_Templates/app.js
@@ -77,10 +77,22 @@ app.use((req, res, next) => {
 });
 
 // 2) Routes
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   res.status(200).render('base', {
     tour: 'The Forest Hiker',
     user: 'Jonas',
+  });
+});
+
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours',
+  });
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
   });
 });
 
